b42bc4822f574967553b7559aac2bb93a7562e19
Tobias Pinisasani <tobiaspisani@gmail.com>
2015-09-21 01:21:48 +0200

    Added avatar to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,21 @@
 class User < ActiveRecord::Base
+  has_many :artworks
+
   before_save :encrypt_password
   after_save :clear_password
-  
+
   attr_accessor :password
-  
+
   EMAIL_REGEX = /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
   validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
   validates :password, :confirmation => true #password_confirmation attr
   validates_length_of :password, :in => 1..20, :on => :create
-  
-  has_attached_file :avatar, styles: { medium: "600x600>", thumb: "300x300>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  mount_uploader :avatar, ImageUploader
 
   def self.authenticate(username_or_email = "", login_password = "")
-    if  EMAIL_REGEX.match(username_or_email)    
+    if  EMAIL_REGEX.match(username_or_email)
       user = User.find_by_email(username_or_email)
     else
       user = User.find_by_username(username_or_email)
@@ -24,11 +25,11 @@ class User < ActiveRecord::Base
     else
       return false
     end
-  end   
+  end
   def match_password(login_password="")
     encrypted_password == BCrypt::Engine.hash_secret(login_password, salt)
   end
-  
+
   private
 
   def encrypt_password
