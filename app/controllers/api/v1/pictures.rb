39f3c4ab7f18cc49d9f5532dcb40d9b1f3df5312
Tobias Pisani <tobiaspisani@gmail.com>
2015-10-26 09:15:38 +0100

    Updated picture controller

--- app/controllers/api/v1/pictures.rb
+++ app/controllers/api/v1/pictures.rb
@@ -1,23 +1,33 @@
 module API
   module V1
-    class Users < Grape::API
+    class Pictures < Grape::API
       include API::V1::Defaults
-
       resource :pictures do
         desc "Return all pictures for a given user"
         params do
-          requires :username, type: String, desc: "Username of the contact"
+          requires :username, type: String, desc: "Username of user to get pictures for"
         end
-        get ":", root: "" do
-          User.all
+        get "user/:username", root: "pictures" do
+          User.where(username: permitted_params[:username]).first.pictures.all
         end
 
-        desc "Return a user"
+        #TODO: Are you allowed to access this picture?
+        desc "Return a picture"
         params do
-          requires :username, type: String, desc: "Username of the contact"
+          requires :id, type: Integer, desc: "ID of picture"
         end
-        get ":username", root: "user" do
-          User.where(username: permitted_params[:username]).first!
+        get ":id", root: "picture" do
+          Picture.find(permitted_params[:id]).first!
+        end
+
+        params do
+          requires :token, type: String, desc: "Authentication token"
+          requires :image, type: File, desc: "Image file"
+          optional :name, type: String, desc: "Name for picture"
+        end
+        post "" do
+          authenticate!
+          Picture.create( image: permitted_params[:image], name: permitted_params[:name], user: current_user )
         end
       end
     end
